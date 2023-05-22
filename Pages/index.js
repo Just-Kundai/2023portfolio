26d6fb7d71c8b505a4386c2257bf4b37a46bce92
Just-Kundai <kundaikhuleya@outlook.com>
2023-05-22 17:21:58 +1000

    index.js

--- Pages/index.js
+++ Pages/index.js
@@ -14,9 +14,9 @@
        <main className=" bg-white px-10">
        <section className="min-h-screen">
          <nav className="py-10 mb-12 flex justify-between">
-         <h1>developedbyK</h1>
+         <h1 className="text-xl">developedbyK</h1>
          <ul className="flex items-center">
-         <li> <BsFillMoonStarsFill/></li>
+         <li> <BsFillMoonStarsFill className="cursor-pointer text-2xl"/></li>
          <li><a href="#"> Resume </a></li>
          </ul>
 
